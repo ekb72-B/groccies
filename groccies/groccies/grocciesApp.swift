9257f2adaded5cafdd82a9de41889a152f1540e5
Esuvat Bomani <esuvatbomani@gmail.com>
2025-01-26 21:58:31 -0500

    New groccies project 🎉

---
+++ groccies/groccies/grocciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  grocciesApp.swift
+//  groccies
+//
+//  Created by ebomani on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct grocciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
